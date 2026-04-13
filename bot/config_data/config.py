fc13ac45d063cdc03a28ad2ebabeed0fc16adfbb
dnedits <dnedits1@gmail.com>
2026-04-13 03:19:31 +0300

    fixes bugs

--- bot/config_data/config.py
+++ bot/config_data/config.py
@@ -1,3 +1,4 @@
+import re
 import os
 from dataclasses import dataclass
 from environs import Env
@@ -17,12 +18,13 @@ def load_config(path: str | None = None) -> Config:
     env = Env()
 
     if path is None:
-        current_dir = os.path.dirname(os.path.abspath(__file__))
-        path = os.path.join(current_dir, '..', '..', '.env')
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        path = os.path.join(base_dir, '..', '..', '.env')
 
     env.read_env(path)
-    return Config(
-        tg_bot=TgBot(
-            token=env('BOT_TOKEN').strip().replace('"', '').replace("'", "")
-        )
-    )
+
+    raw_token = env('BOT_TOKEN')
+
+    clean_token = re.sub(r'[^a-zA-Z0-9:-]', '', raw_token)
+
+    return Config(tg_bot=TgBot(token=clean_token))
